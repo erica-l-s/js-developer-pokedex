35cf25605d9b97273d7f314d0f9cafd1791898fb
erica-l-s <erica.l.s1996@gmail.com>
2023-04-05 23:01:05 -0300

    terminando css detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const pokemonDetail = document.getElementById('data')
+
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -35,10 +36,66 @@ const selectPokemon = async (id) =>{
 }
 
 const displayPopup = (pokemon) =>{
-    const type = pokemon.types.map((type)=>
-    type.type.name).join(',')
-    console.log(type)
+   
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
 
+    pokemon.types = types
+    pokemon.type = type
+ 
+    const stats = pokemon.stats.map((base_stats)=>base_stats.base_stat)
+    const name_stats = pokemon.stats.map((name)=>name.stat.name)
+    const photo = pokemon.sprites.other.dream_world.front_default
+    const htmlString = `
+   
+    <div id="popup">
+      <div id="detailPokemon">
+          <button id="closeBtn" onClick="closePopup()">Close</button>
+            <li class="pokemon ${pokemon.type}">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">#${pokemon.id.toString().padStart(3,"0")}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+            </div>
+                 <img id="img-pokemon" src="${photo}"alt="${pokemon.name}">
+                
+                 <div id="data">
+                 <h4>Base Stats</h4>
+                 <div id="stats">
+                         
+                         <div class="stat-desc">${name_stats}</div>
+                         <div class="bar-inner">${stats}</div>
+                         
+                         <p>Height: ${pokemon.height}</p>
+                         <p>Weight: ${pokemon.weight}</p>
+                         <div class="stat-bar">
+                     
+                         
+                     </div>
+                </div>
+             </div>
+           </div>
+        </div>
+        
+      
+          </li>
+          
+            
+             
+                
+            
+    
+    `
+   
+    pokemonList.innerHTML = htmlString + pokemonList.innerHTML
+}
+
+const closePopup = () =>{
+    const popup = document.getElementById('popup')
+    popup.parentElement.removeChild(popup)
 }
 
 function loadPokemonItens(offset, limit) {
